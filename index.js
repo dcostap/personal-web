11d6b37db61350f4ceb78b1cedf4f691f1557e59
Dario Costa Pena <Dario Costa@CASTROSUA.COM>
2022-04-28 16:25:30 +0200

    Light mode. Needs more style & color tweaks

--- index.js
+++ index.js
@@ -33,3 +33,48 @@ function checkAnimation() {
 $(window).scroll(function(){
     checkAnimation();
 });
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const observer = new MutationObserver(list => {
+        const evt = new CustomEvent('dom-changed', { detail: list });
+        document.documentElement.dispatchEvent(evt)
+    });
+    observer.observe(document.documentElement, { attributes: true, childList: true, subtree: true });
+
+    document.documentElement.addEventListener("dom-changed", e => console.log(e));
+
+    updateTheme();
+})
+
+function updateTheme() {
+    // check for the OS theme setting
+    let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: light)");
+
+    // setting that overrides OS theme, so user can change it
+    let theme = sessionStorage.getItem('theme');
+
+    if ((theme != null && theme == "dark") || (theme == null && systemInitiatedDark.matches)) {
+        document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
+        sessionStorage.setItem('theme', 'dark');
+    } else {
+        document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
+        sessionStorage.setItem('theme', 'light');
+    }
+
+    // if the OS changes theme, we override any configuration to match it
+    systemInitiatedDark.addListener(function () {
+        sessionStorage.clear();
+        updateTheme();
+    });
+}
+
+function themeSwitcher() {
+    let theme = sessionStorage.getItem('theme');
+    if (theme == null || theme == "light")
+        sessionStorage.setItem('theme', 'dark');
+    else
+        sessionStorage.setItem('theme', 'light');
+
+    updateTheme();
+}
